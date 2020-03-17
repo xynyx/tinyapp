117f5bb598b6dd69ddc438297ed2c1dae9772f3c
Matt Taylor <matt.taylr22@gmail.com>
2020-03-17 22:18:36 +0000

    added basic cookie username functionality

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 function generateRandomString() {
   return Math.random().toString(36).slice(2).substring(0, 6);
@@ -40,14 +42,13 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
-})
+});
 
 // Update longURL
 app.post("/urls/:shortURL", (req, res) => {
-  console.log(req.body);
-  urlDatabase[req.params.shortURL] = req.body.longURL ;
+  urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect("/urls");
-})
+});
 
 // After generating new shortURL, and using route parameters, redirect user to urls_show page displaying the short and long URL
 app.get("/urls/:shortURL", (req, res) => {
@@ -57,16 +58,21 @@ app.get("/urls/:shortURL", (req, res) => {
 
 // When user inputs new URL, a random string is generated for the shortURL, and a shortURL:longURL pair is created and added to urlDatabase
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString()
+  const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`)
+  res.redirect(`/urls/${shortURL}`);
 });
 
 // When the shortened link is clicked on after redirection, redirect to the longURL site
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
-})
+});
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
