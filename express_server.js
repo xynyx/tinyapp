901b5246de84b2b1eab6484dfa4ce0b9dd757fbb
Matt Taylor <matt.taylr22@gmail.com>
2020-03-17 17:00:27 +0000

    Added ability to delete created links and refresh the page

--- express_server.js
+++ express_server.js
@@ -43,6 +43,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+})
 // After generating new shortURL, and using route parameters, redirect user to urls_show page displaying the short and long URL
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
