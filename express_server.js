d33feebe733211da1df0eac843dda0774b472d96
Matt Taylor <matt.taylr22@gmail.com>
2020-03-20 17:02:27 +0000

    updated several files, cleaned code

--- express_server.js
+++ express_server.js
@@ -3,29 +3,22 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-var cookieSession = require('cookie-session');
+const cookieSession = require('cookie-session');
 app.use(cookieSession({
   name: "session",
   keys: ["user_id"]
 }));
 const bcrypt = require("bcrypt");
-app.listen(PORT, () => { });
+const methodOverride = require("method-override");
+app.use(methodOverride('_method'));
+
 const { getUserByEmail, generateRandomString, urlsForUser } = require("./helpers.js");
 
+app.listen(PORT, () => { });
 app.set("view engine", "ejs");
 
 const urlDatabase = {};
 const users = {};
-
-// urlDatabase in json format (REMOVE)
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-// Users json for testing (REMOVE)
-app.get("/users.json", (req, res) => {
-  res.json(users);
-});
 
 // Create new tiny URL
 app.get("/urls/new", (req, res) => {
@@ -41,7 +34,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 // Delete entry added to My URLs
-app.post("/urls/:shortURL/delete", (req, res) => {
+app.delete("/urls/:shortURL", (req, res) => {
   const user = users[req.session.user_id];
   const shortURL = req.params.shortURL;
   if (user.id === urlDatabase[shortURL].userID) {
@@ -53,7 +46,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 // Update longURL
-app.post("/urls/:shortURL", (req, res) => {
+app.put("/urls/:shortURL", (req, res) => {
   const user = users[req.session.user_id];
   const shortURL = req.params.shortURL;
   if (urlsForUser(user.id, urlDatabase)[shortURL].userID === urlDatabase[shortURL].userID) {
@@ -64,19 +57,50 @@ app.post("/urls/:shortURL", (req, res) => {
   }
 });
 
-// After generating new shortURL, and using route parameters, redirect user to urls_show page displaying the short and long URL
+// After generating new shortURL, redirect user to urls_show page displaying the short and long URL
 app.get("/urls/:shortURL", (req, res) => {
   const user = users[req.session.user_id];
   const shortURL = req.params.shortURL;
-  const userObject = urlsForUser(user.id, urlDatabase)[shortURL]
+  const userObject = urlsForUser(user.id, urlDatabase)[shortURL];
+  const linkData = urlDatabase[shortURL];
   // Check filtered list whether a shortURL exists (ie. not undefined), and continue
   if (user && userObject !== undefined) {
     let templateVars = {
+      linkData,
+      visits: linkData.visits,
+      uniqueVisits: linkData.uniqueVisits,
       user,
       shortURL: shortURL,
       longURL: userObject.longURL,
     };
+
     res.render("urls_show", templateVars);
+  } else {
+    res.redirect("/login");
+  }
+});
+// Generate random shortURL code and attach longURL to it
+app.post("/urls", (req, res) => {
+  const user = users[req.session.user_id];
+  const shortURL = generateRandomString();
+  // TinyApp will not function correctly without proper protocol; add if missing
+  if (!req.body.longURL.includes("http://")) {
+    urlDatabase[shortURL] = { longURL: `http://${req.body.longURL}`, userID: user.id, visits: 0, uniqueVisits: 0, visitorID: [], timeStamp: [] };
+  } else {
+    urlDatabase[shortURL] = { longURL: req.body.longURL, userID: user.id, visits: 0, uniqueVisits: 0, visitorID: [], timeStamp: [] };
+  }
+  res.redirect(`/urls/${shortURL}`);
+});
+
+// Displays the index which is a list of the urlDatabase shortURL:longURL pairs
+app.get("/urls", (req, res) => {
+  const user = users[req.session.user_id];
+  if (user) {
+    let templateVars = {
+      urls: urlsForUser(user.id, urlDatabase),
+      user
+    };
+    res.render("urls_index", templateVars);
   } else {
     res.redirect("/login");
   }
@@ -86,6 +110,7 @@ app.get("/register", (req, res) => {
   res.render("urls_register", { user: undefined });
 });
 
+// Register the user, checking for validity
 app.post("/register", (req, res) => {
   if (req.body.email === "" || req.body.password === "") {
     res.status(400).send('Email and/or password field cannot be left blank.');
@@ -103,52 +128,34 @@ app.post("/register", (req, res) => {
   }
 });
 
-// Generate random 6-digit shortURL code and attach longURL to it
-app.post("/urls", (req, res) => {
-  const user = users[req.session.user_id];
-  const shortURL = generateRandomString();
-  // TinyApp will not function correctly without proper protocol; add if missing
-  if (!req.body.longURL.includes("http://")) {
-    urlDatabase[shortURL] = { longURL: `http://${req.body.longURL}`, userID: user.id };
-  } else {
-    urlDatabase[shortURL] = { "longURL": req.body.longURL, "userID": user.id };
-  }
-  res.redirect(`/urls/${shortURL}`);
-});
-
-// Displays the index which is a list of the urlDatabase shortURL:longURL pairs
-app.get("/urls", (req, res) => {
-  const user = users[req.session.user_id];
-  if (user) {
-    let templateVars = {
-      urls: urlsForUser(user.id, urlDatabase),
-      user
-    };
-    res.render("urls_index", templateVars);
-  } else {
-    res.redirect("/register");
-  }
-});
-
-// When the shortened link is clicked on, redirect to the site`http://${req.body.longURL}`
+// When the shortened link is clicked on, redirect to the site
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
+  const shortURL = req.params.shortURL;
+  const linkData = urlDatabase[shortURL];
+  linkData.visits++;
+  if (!req.session.unique) {
+    req.session.unique = generateRandomString();
+    linkData.uniqueVisits++;
+  }
+  linkData.visitorID.push(req.session.unique);
+  linkData.timeStamp.push(Date());
   res.redirect(longURL);
 });
 
 // Login and create cookie for current user
 app.post("/login", (req, res) => {
   const user = getUserByEmail(req.body.email, users);
-  const encryptedPasswordMatch = bcrypt.compareSync(req.body.password, users[user].password);
   if (user) {
-    if (users[user].email === req.body.email && encryptedPasswordMatch) {
+    if (users[user].email === req.body.email && bcrypt.compareSync(req.body.password, users[user].password)) {
       req.session.user_id = users[user].id;
       res.redirect("/urls");
       // Returning here will prevent "Can't set headers after they are sent" error
       return;
     }
+    res.status(403).send('The email and/or password is incorrect.');
   }
-  res.status(403).send('The email and/or password is incorrect.');
+  res.status(403).send('The account does not exist. Please create an account.');
 });
 
 app.get("/login", (req, res) => {
@@ -157,7 +164,7 @@ app.get("/login", (req, res) => {
 
 // Logout and delete cookie for current user
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id", users[req.session.user_id]);
+  req.session = null;
   res.redirect("/login");
 });
 
